6ba5e72ff726ddf4423e2a43af96d5188f24458c
ToporekDaniel <danieltoporek0@gmail.com>
2023-11-25 23:12:45 +0100

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+for (const item of galleryItems) {
+  const pic = `<li>
+    <a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+</li>`;
+
+  gallery.insertAdjacentHTML('beforeend', pic);
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const lightbox = new SimpleLightbox('.gallery a', {
+//     captionsData: 'alt',
+//     captionDelay: 250,
+//   });
+// });
